623141d56727d1a2e7775d359fad6ec6b2fdbb59
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-11-27 16:34:07 +0600

    ng routing and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { StudentListComponent } from './student-list/student-list.component';
+// import { DepartmentListComponent } from './department-list/department-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // StudentListComponent,
+    // DepartmentListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
